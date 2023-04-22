4af57d550a1f8ab93fedc9b966fcb80102535ccc
Jean Paul Mayorga <jpmayorga@outlook.com>
2023-04-22 10:00:53 -0500

    Initial Commit

---
+++ personal/personalApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalApp.swift
+//  personal
+//
+//  Created by Jean Mayorga on 22/4/23.
+//
+
+import SwiftUI
+
+@main
+struct personalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
